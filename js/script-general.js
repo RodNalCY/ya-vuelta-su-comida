7763c173ddde4a30cc476c45d084d4d1ef7318c2
RodNalCY <rodnalcabello07@gmail.com>
2022-05-21 22:19:23 -0500

    Se optimizo la web seccion de pagos

--- js/script-general.js
+++ js/script-general.js
@@ -4,7 +4,7 @@ $(document).ready(function () {
   
   html_global_price = html_global_price + "<sup>S/ </sup><span>0.<small>00</small></span>";
   $(".price").html(html_global_price);
-  $('.audio-properties')[0].play();
+  // $('.audio-properties')[0].play();
 
 
   $("#btnComprar").click(function () {
@@ -31,4 +31,60 @@ $(document).ready(function () {
     html_global_price = html_global_price + "<sup>S/ </sup><span>25.<small>00</small></span>";
     $(".price").html(html_global_price);
   });
+
+  $("#btnPagar").click(function () {
+    $("#mdRegistroCompra").modal("hide");
+    $("#mdMetodoPagar").modal("show");    
+  });
+
+
+  $("#mpPlin").click(function () {
+    console.log("Plin");
+    $("#mdMetodoPagar").modal("hide"); 
+    $("#mdQRPago").modal("show");   
+    $("#img-qr-pago").attr("src","https://i.blogs.es/f1c47c/xatakamovilqr/450_1000.jpg");
+
+  });
+
+  $("#mpYape").click(function () {
+    console.log("Yape");
+    $("#mdMetodoPagar").modal("hide"); 
+    $("#mdQRPago").modal("show"); 
+    $("#img-qr-pago").attr("src","https://cdn.urbantecno.com/rootear/2015/07/qr.jpg");
+
+  });
+
+  $("#mpContraEntrega").click(function () {
+    console.log("Contra Entrega");
+    $("#mdMetodoPagar").modal("hide"); 
+    $("#mdQRPago").modal("show"); 
+    $("#img-qr-pago").attr("src","https://conectasoftware.com/wp-content/uploads/2020/04/conecta-software-codigo-qr.png");
+  });
+
+  $("#btnIrAtras").click(function () {
+    $("#mdMetodoPagar").modal("show"); 
+    $("#mdQRPago").modal("hide"); 
+  });
+
+  $("#btnIrAtrasFormulario").click(function () {
+    $("#mdRegistroCompra").modal("show");
+    $("#mdMetodoPagar").modal("hide");
+  });
+  
+
+  $("#btnFinalizarCompra").click(function () {
+    $("#mdMetodoPagar").modal("hide");
+    Swal.fire({
+      icon: 'success',
+      title: 'Uy, bien!',
+      text: 'reserva realizada con Exito',
+      showConfirmButton: false,
+      timer: 2000
+    });
+  });
+  
+  
+
+  
+
 });
